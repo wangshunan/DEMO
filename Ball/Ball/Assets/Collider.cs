ec8f39a3feddb99b981d5b71cdaf717235fd0549
wangshunan <wangshunan12@gmail.com>
2017-10-07 15:41:09 +0900

    test

---
+++ Ball/Ball/Assets/Collider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider : MonoBehaviour {
+
+    [SerializeField]
+    Gravity gravity;
+
+    public GameObject cube;
+
+    void Start () {
+        gravity = GetComponent<Gravity>();
+	}
+
+	void Update () {
+
+        float dis;
+        float r = transform.localScale.y / 2;
+        float cubeR = cube.transform.localScale.y / 2;
+        float offset = r + cubeR;
+
+        dis = Vector3.Distance(transform.position, cube.transform.position);
+
+        /*if ( dis <= offset )
+        {
+            transform.position = new Vector3(transform.position.x, cube.transform.position.y + offset, transform.position.z);
+            gravity.onGravity = false;
+        }*/
+
+        Vector3 tmp = cube.transform.position - gameObject.transform.position;
+
+        float angle = Vector3.Angle(cube.transform.position.normalized, tmp);
+
+        Debug.Log(angle);
+        Debug.DrawLine(cube.transform.position, gameObject.transform.position);
+        //Debug.DrawLine(transform.position, transform.position.normalized);
+        //Debug.DrawLine(cube.transform.position, cube.transform.up * 2);
+    }
+}
---
+++ Ball/Ball/Assets/Gravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+	
+    private float gravity;
+    private float fallSpeed;
+    
+    public bool onGravity;
+
+    private void Start()
+    {
+        //onGravity = true;
+        gravity = 9.8f;
+        fallSpeed = 0;
+    }
+
+    void Update () {
+
+        if ( !onGravity )
+        {
+            return;
+        }
+
+        fallSpeed -= gravity * Time.deltaTime;
+
+        gameObject.transform.position += new Vector3(0, fallSpeed * Time.deltaTime, 0);
+
+	}
+}
